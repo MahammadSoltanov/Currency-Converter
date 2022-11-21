ef86ef11e03306786c0d7df219a59dbb8074e238
MaqaSoltanov <113199901+MaqaSoltanov@users.noreply.github.com>
2022-11-21 15:44:14 +0400

    Wrote logic for currency selector

--- mainLogic.js
+++ mainLogic.js
@@ -3,8 +3,7 @@ function mainLogic(ifMobile)
 {    
     // Mobile logic
     if(ifMobile.matches)
-    {
-        console.log(`works`);
+    {        
         header.innerHTML = `<img src="/images/logo.svg">
         
         <div class="collapsible">
@@ -24,8 +23,7 @@ function mainLogic(ifMobile)
     }
     // Tablet and PC logic
     else
-    {
-        console.log(`works 2`);
+    {        
         header.innerHTML = `<img src="/images/logo.svg">
 
         <div class="menu-container">
@@ -50,3 +48,74 @@ window.addEventListener('resize', function(event) {
 }, true);
 //#endregion
 
+let firstCurrency, secondCurrency;
+firstCurrency = 'RUB';
+secondCurrency = 'USD';
+
+const currencyButtons = document.getElementsByClassName('currency-value');
+changeColor(0); changeColor(5);
+
+for(let i = 0; i < currencyButtons.length; i++){currencyButtons[i].addEventListener('click', function(e){changeColor(e,i);});}
+
+const userInputs = document.getElementsByClassName('user-input');
+userInputs[0].addEventListener('input', convertMoney(from, to));
+userInputs[1].addEventListener('input', convertMoney(from, to));
+
+const currencyInfos = document.getElementsByClassName('currency-info');
+
+function changeColor(e, index)
+{   
+    if(index < 4)
+    {
+        firstCurrency = currencyButtons[index].textContent;
+        updateCurrencyInfo(0);    
+        currencyButtons[index].style.backgroundColor = '#833AE0';
+        currencyButtons[index].style.color = 'white';
+
+        for(let i = 0; i < 4; i++)
+        {
+            if(i != index)
+            {
+                currencyButtons[i].style.backgroundColor = 'white';
+                currencyButtons[i].style.color = '#9F9F9F';
+            }
+        }
+    }
+
+    else
+    {
+        secondCurrency = currencyButtons[index].textContent;
+        updateCurrencyInfo(1);    
+        currencyButtons[index].style.backgroundColor = '#833AE0';
+        currencyButtons[index].style.color = 'white';
+
+        for(let i = 4; i < 8; i++)
+        {
+            if(i != index)
+            {
+                currencyButtons[i].style.backgroundColor = 'white';
+                currencyButtons[i].style.color = '#9F9F9F';
+            }
+        }
+    }
+}
+
+function convertMoney(from, to)
+{
+
+}
+
+function updateCurrencyInfo(index)
+{
+    
+}
+
+var requestURL = 'https://api.exchangerate.host/convert?from=USD&to=EUR&amount=1200&places=4';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+
+
+        
